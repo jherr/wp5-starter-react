76f5a8019c79172e86a24690b7ae63dde9403b13
Jack Herrington <jherr@pobox.com>
2020-06-22 16:54:58 -0700

    Re-adding env

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from React.</div>;
+const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
